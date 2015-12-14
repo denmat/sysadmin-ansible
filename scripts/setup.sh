76c14dc81ac8ed4c6795ab479ce886853c4cb1cc
DenMat <dennis.matotek@envato.com>
2015-12-15 08:40:52 +1100

    trying to put this in script as ci rake tasks don't work

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+PATH=/opt/boxen/rbenv/shims/bundle:/opt/boxen/homebrew/bin:/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/bin:/sbin
+
+rbenv shell 2.2.3
+
 eval $(docker-machine env dinghy)
 
-PATH=/opt/boxen/rbenv/shims:$PATH
-
 bundle check || bundle install --path vendor/bundle --binstubs bin -j4
+
+bundle exec rake docker:build
+bundle exec rake docker:run
+bundle exec rake spec
+bundle exec rake docker:clean
